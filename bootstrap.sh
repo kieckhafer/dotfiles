b95376a22e0370cfa2bd2254c40b51986276d648
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-07-25 22:37:15 -0700

    move dotfiles back to ~

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~/.dotfiles;
+		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
