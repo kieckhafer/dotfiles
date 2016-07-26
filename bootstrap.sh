05c398f0c2f6f6cc6104e5de7338e4b7f11e081c
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-07-25 22:34:09 -0700

    move dotfiles into .dotfiles directory to keep ~ clean(er)

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
+		-avh --no-perms . ~/.dotfiles;
 	source ~/.bash_profile;
 }
 
